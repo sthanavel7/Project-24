231ce07fc100a1f092bec41bf52dcdf67ff4620a
sthanavel7 <57311048+sthanavel7@users.noreply.github.com>
2020-11-22 16:02:59 +0530

    Add files via upload

---
+++ Dustbin.js
@@ -0,0 +1,13 @@
+class DustBin {  
+    constructor( x, y, width, height){
+        var options = {
+            'isStatic' : true
+        }
+   }
+   display(){
+       fill("white");
+       rect(650,395,200,10)
+       rect(550,360,10,150)
+       rect(750,360,10,150)
+   }
+}
